7bd50347abb0174e9ccd15f4155e75c4e85895c7
mahikansal <mahikansal916@gmail.com>
2026-07-31 23:22:35 +0530

    Add elements

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,8 +1,13 @@
 import Chai from "./Chai"
 
 function App() {
+  const username = "chai aur code"
   return (
+    <>
     <Chai/>
+    <h1>chai aur react {username}</h1>
+    <p>test para</p>
+    </>
   )
 }
 
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,47 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp() {
+    return (
+        <div>
+            <h1>Custom App!!</h1>
+        </div>
+    )
+}
+
+// const ReactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit google'
+// }
+
+const anotherElement = (
+    <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+
+const anotherUser = " chai aur react"
+
+const reactElement = React.createElement(
+    'a',
+    {href:"https://google.com", target: "_blank"},
+    'click me to visit google',
+    anotherUser
+)
+
 createRoot(document.getElementById('root')).render(
+
+    // <App/>
   
-    <App />
+    // MyApp()  as it is a function we can call it like this
+
+    // <MyApp/>
+
+    // anotherElement
+
+    reactElement
   
 )
